18fdb36600cc60f1a1dd44a4920824839164c5bf
Ratana <chhormratana@gmail.com>
2026-08-02 13:57:34 +0700

    docs: fix redirect url

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 
-export default function ToIndex(): JSX.Element {
+import styles from './index.module.css';
+
+const DOCS_QUICK_START = 'docs/guide/quickStart/start';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <BrowserOnly>
-      {() => {
-        /**
-         * Redirect to the home page
-         */
-        window.location.href = '/index';
-        return <></>;
-      }}
-    </BrowserOnly>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
+        <h2>Welcome to the Low-Code Engine Docs</h2>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to={DOCS_QUICK_START}>
+            Quick Start
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={siteConfig.title} description="">
+      <HomepageHeader />
+    </Layout>
   );
 }
